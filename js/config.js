dc550892b6f517b182f5682fc6e8f8914696c2e3
Gibson <58445362+GibsonFR@users.noreply.github.com>
2026-04-24 02:57:15 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://wbyxxpqxlsmjyphsmcqm.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndieXh4cHF4bHNtanlwaHNtY3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNTMzMjAsImV4cCI6MjA3NzkyOTMyMH0.v2s-8zcMZKS4ourgorpAj0SMbPUoPvIhdxYyTNkYuPs";
+const SUPABASE_URL = "https://kturlmcgsaitwvqgevnj.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0dXJsbWNnc2FpdHd2cWdldm5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NzUyNDYsImV4cCI6MjA5MjU1MTI0Nn0.exdRK9SQb7E7PkPrvUO0T6LYUj8CbkpvrYyxPD8ipH8";
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 window.SUPABASE_URL = SUPABASE_URL;
